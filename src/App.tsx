86be9c1cbb14d5841ff3f28cc502b6eb1715e683
Abdullah <AbAlomari95@gmail.com>
2023-02-10 18:40:55 +0300

    Uploading file V2

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,20 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import WeatherApi from './components/WeatherApi'
 import { ChakraProvider } from '@chakra-ui/react'
+import { Router, Route, Routes } from 'react-router-dom'
+
 
 function App() {
 
   return (
     // hello
+    // <Routes>
     <ChakraProvider>
-    <div className="App">
-      <WeatherApi/>
-    </div>
+      
+      <WeatherApi/>      
+    
     </ChakraProvider>
+    /* </Routes> */
   )
 }
 
